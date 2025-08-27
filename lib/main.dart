c0b88f9c5d1417061484d45386655635d8fb8650
hyperbora <hyperbora@gmail.com>
2025-08-27 21:03:02 +0900

    feat: firebase 설정 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:coupon_place/firebase_options.dart';
 import 'package:coupon_place/src/app.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await signInAnonymously();
   runApp(const ProviderScope(child: MyApp()));
+}
+
+Future<User?> signInAnonymously() async {
+  UserCredential userCredential =
+      await FirebaseAuth.instance.signInAnonymously();
+  return userCredential.user;
 }
