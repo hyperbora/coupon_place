14f2ba4f7c646d90ca3d22c1e5b4b8667f1e91e4
hyperbora <hyperbora@gmail.com>
2025-09-05 21:17:39 +0900

    fix: 폴더 상세화면에서 각 쿠폰에 슬라이드 메뉴 추가, 쿠폰 등록 버튼 추가

--- lib/src/features/coupon/view/folder_detail_screen.dart
+++ lib/src/features/coupon/view/folder_detail_screen.dart
@@ -1,7 +1,10 @@
 import 'package:coupon_place/src/features/coupon/view/coupon_list_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'coupon_register_screen.dart';
 import '../provider/coupon_list_provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FolderDetailScreen extends ConsumerWidget {
   final String folderId;
@@ -15,6 +18,7 @@ class FolderDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final loc = AppLocalizations.of(context)!;
     final coupons =
         ref
             .watch(couponListProvider)
@@ -27,8 +31,52 @@ class FolderDetailScreen extends ConsumerWidget {
         itemCount: coupons.length,
         itemBuilder: (context, index) {
           final coupon = coupons[index];
-          return CouponListItem(coupon: coupon);
+          return Slidable(
+            key: ValueKey(coupon.id),
+            endActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (context) {
+                    // TODO: 수정 화면 이동 (쿠폰 수정 폼 연결)
+                  },
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                  icon: Icons.edit,
+                  label: loc.edit,
+                ),
+                SlidableAction(
+                  onPressed: (context) {
+                    // TODO: 쿠폰 삭제 로직 추가
+                  },
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  icon: Icons.delete,
+                  label: loc.delete,
+                ),
+              ],
+            ),
+            child: CouponListItem(coupon: coupon),
+          );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            isScrollControlled: true,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+            ),
+            builder:
+                (context) => FractionallySizedBox(
+                  heightFactor: 0.9,
+                  child: const CouponRegisterScreen(),
+                ),
+          );
+        },
+        tooltip: loc.couponRegisterTooltip,
+        child: const Icon(Icons.add),
       ),
     );
   }
