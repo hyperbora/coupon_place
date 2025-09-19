c59da482705a8ddb6e5085085a096dbe76eab477
hyperbora <hyperbora@gmail.com>
2025-09-19 22:15:33 +0900

    feat: 앱 초기에 알림 권한 요청 추가, 알림 관련 패키지 추가

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,17 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart' as tz;
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await signInAnonymously();
+  await initNotifications();
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -16,4 +22,32 @@ Future<User?> signInAnonymously() async {
   UserCredential userCredential =
       await FirebaseAuth.instance.signInAnonymously();
   return userCredential.user;
+}
+
+Future<void> initNotifications() async {
+  const AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  const DarwinInitializationSettings initializationSettingsDarwin =
+      DarwinInitializationSettings(
+        requestAlertPermission: true,
+        requestBadgePermission: true,
+        requestSoundPermission: true,
+      );
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: (NotificationResponse response) {
+      debugPrint("알림 클릭됨, payload: ${response.payload}");
+      print("알림 클릭됨, payload: ${response.payload}");
+      // TODO: 여기서 네비게이션 처리 (쿠폰 상세 화면 이동)
+    },
+  );
+
+  tz.initializeTimeZones();
 }
