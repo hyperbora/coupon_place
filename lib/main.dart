d330c4578f324fd907e2fba74359e3f08f9ebef6
hyperbora <hyperbora@gmail.com>
2025-09-27 22:59:16 +0900

    fix: firestore 관련 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:coupon_place/firebase_options.dart';
 import 'package:coupon_place/src/app.dart';
 import 'package:coupon_place/src/features/coupon/model/coupon_model.dart';
 import 'package:coupon_place/src/features/folder/model/folder_model.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -15,8 +12,6 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await signInAnonymously();
   await initNotifications();
   await Hive.initFlutter();
   Hive.registerAdapter(CouponAdapter());
@@ -24,12 +19,6 @@ void main() async {
   await Hive.openBox<Coupon>('coupons');
   await Hive.openBox<Folder>('folders');
   runApp(const ProviderScope(child: MyApp()));
-}
-
-Future<User?> signInAnonymously() async {
-  UserCredential userCredential =
-      await FirebaseAuth.instance.signInAnonymously();
-  return userCredential.user;
 }
 
 Future<void> initNotifications() async {
