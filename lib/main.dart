e8b8807252b01a87c9591495c5cb6dd5d6630f7a
hyperbora <hyperbora@gmail.com>
2025-09-30 16:50:08 +0900

    feat: 최초 실행시 기본 폴더 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:coupon_place/src/app.dart';
 import 'package:coupon_place/src/features/coupon/model/coupon_model.dart';
 import 'package:coupon_place/src/features/folder/model/folder_model.dart';
+import 'package:coupon_place/src/features/folder/provider/folder_provider.dart';
+import 'package:coupon_place/src/infra/prefs/shared_preferences_keys.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
@@ -18,6 +21,7 @@ void main() async {
   Hive.registerAdapter(FolderAdapter());
   await Hive.openBox<Coupon>('coupons');
   await Hive.openBox<Folder>('folders');
+  await addDefaultFolderIfPossible();
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -49,4 +53,18 @@ Future<void> initNotifications() async {
   );
 
   tz.initializeTimeZones();
+}
+
+Future<void> addDefaultFolderIfPossible() async {
+  final prefs = await SharedPreferences.getInstance();
+  final isFirstLaunch =
+      prefs.getBool(SharedPreferencesKeys.firstLaunchKey) ?? true;
+
+  if (isFirstLaunch) {
+    final container = ProviderContainer();
+    container
+        .read(folderProvider.notifier)
+        .addFolder('My Folder', Colors.blue, Icons.folder);
+    await prefs.setBool(SharedPreferencesKeys.firstLaunchKey, false);
+  }
 }
